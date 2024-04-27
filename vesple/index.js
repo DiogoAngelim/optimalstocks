6a50e7eccf19a74da3c718a47bc64505f58ffe65
Diogo <diogoangelim@gmail.com>
2024-04-27 07:51:28 -0300

    Enabled cron job

--- vesple/index.js
+++ vesple/index.js
@@ -1,14 +1,12 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
+const cron = require('node-cron');
+
 
 let rawdata = fs.readFileSync('stocks_list_BR.json');
 let assets = JSON.parse(rawdata);
 
 async function fetchData(symbol) {
-  var path = `data/${symbol}.csv`;
-  // var stat = fs.statSync(path);
-
-  // if ( stat && stat.size < 1000 ) {
 
     try {
       let result;
@@ -17,17 +15,26 @@ async function fetchData(symbol) {
       result = await fetch( `https://query1.finance.yahoo.com/v7/finance/download/${symbol}?period1=1657465580&period2=1696901586&interval=1d&events=history&includeAdjustedClose=true`);
       data = await result.text(); 
 
-      fs.writeFileSync(`data/${symbol}.csv`, data, { encoding: 'utf8', flag: 'w' });
+      fs.writeFileSync(`../data/${symbol}.csv`, data, { encoding: 'utf8', flag: 'w' });
     } catch (error) {
       console.log('retrying ', symbol);
-      fetchData(symbol);
+      var path = `../data/${symbol}.csv`;
+      var stat = fs.statSync(path);
+
+      if ( stat && stat.size < 1000 ) {
+        fetchData(symbol);
+      } 
     }
-  // }
 }
 
-for (let index = 0; index < assets.length; index++) {
-  const symbol = assets[index].symbol;
 
-  fetchData(symbol);
+cron.schedule('0 22 * * 1-5', () => {
+  console.log('At 22:00 on every day-of-week from Monday through Friday.');
 
-}
+  for (let index = 0; index < assets.length; index++) {
+    const symbol = assets[index].symbol;
+  
+    fetchData(symbol);
+  
+  }
+});
